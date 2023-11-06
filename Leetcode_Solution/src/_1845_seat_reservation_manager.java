6d3445941dd999c558b95f0580148cb4548d4685
Son Nguyen Xuan <Son.Nguyen.20022020@gmail.com>
2023-11-06 21:26:09 +0700

    add

---
+++ Leetcode_Solution/src/_1845_seat_reservation_manager.java
@@ -0,0 +1,20 @@
+public class _1845_seat_reservation_manager {
+    public _1845_seat_reservation_manager(int n) {
+        
+    }
+
+    public int reserve() {
+        return 1;
+    }
+
+    public void unreserve(int seatNumber) {
+
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        _1845_seat_reservation_manager obj = new _1845_seat_reservation_manager(5);
+
+
+    }
+}
